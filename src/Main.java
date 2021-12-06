8015172f7d5bfd0a84f6de0111e0cbde7c20f0f2
Vladimir Protcay <protcay2@mail.ru>
2021-12-06 12:40:46 +0300

    исправлено

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int amount = 10000;
+        int miles = 20;
+        int bonus = amount / miles;
+        System.out.println("потраченная сумма:" + amount);
+        System.out.println("бонусных миль начислено:" + bonus);
+
+    }
+}
